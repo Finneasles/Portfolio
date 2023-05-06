c84e224f2ccacd0e55a7b7568c63a67c07c806d5
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-06 22:37:55 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,23 @@
 import { useScrollProgress } from "@/hooks";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import React from "react";
+import React, { useEffect } from "react";
 
 const BodyComponent = ({ children }: { children: any }) => {
   const { scrollY } = useScrollProgress();
+
+  useEffect(() => {
+    function setStatusBarColor() {
+      const meta = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
+      if (!meta) return;
+      const color = scrollY > 100 ? '#00162D' : '#5FA7FC';
+      meta.setAttribute('content', color);
+    }
+
+    setStatusBarColor();
+  }, [scrollY]);
+
   return (
     <body
-      className={`${
-        scrollY > 100 ? "dark:bg-[#101727]" : "dark:bg-[#408FED]"
-      } bg-white bg-gradient-to-b text-black  dark:from-[#00162D] dark:to-[#000] dark:text-white`}
     >
       {children}
     </body>
@@ -30,3 +39,6 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+function useEffects(arg0: () => void, arg1: undefined[]) {
+  throw new Error("Function not implemented.");
+}

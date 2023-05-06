be8cc78981f2ea5c818bd00e6234abe0401e4ce2
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-06 22:35:15 +0100

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,12 +4,17 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   en: {
     translation: {
-      hero_subtitle0: "Hello! I'm a",
-    },
-  },
-  fr: {
-    translation: {
-      greeting: "Bonjour, {{name}} !",
+      home_label: "Home",
+      about_label: "About",
+      projects_label: "Projects",
+      pubs_label: "Publications",
+      signIn_label: "Sign in",
+      hireMe_label: "Hire me",
+      contactMe_label: "Contact me",
+      hero_span0: "Hello! I'm a",
+      hero_subtitle0: "Developer",
+      hero_desc0:
+        "Skilled in web development technologies such as React, Node.js & TypeScript.",
     },
   },
 };

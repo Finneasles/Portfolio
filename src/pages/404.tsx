b707d98a7c08c1e724f81d87a5fe132b75b00e0c
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-05-08 09:37:50 +0100

    :zap: update pages
    404 & _doc

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,16 +2,7 @@ import { Four0FourPage } from "@/components";
 import { getAllPostsWithFrontMatter } from "@/utils";
 import { useRouter } from "next/router";
 
-export default function Home({ postsData }) {
+export default function _404() {
   const router = useRouter();
-  return <Four0FourPage posts={postsData} router={router}/>;
-}
-
-export async function getStaticProps() {
-  const postsData = await getAllPostsWithFrontMatter("posts");
-  return {
-    props: {
-      postsData,
-    },
-  };
+  return <Four0FourPage router={router}/>;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,43 +2,18 @@ import { useScrollProgress } from "@/hooks";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React, { useEffect } from "react";
 
-const BodyComponent = ({ children }: { children: any }) => {
-  const { scrollY } = useScrollProgress();
-
-  useEffect(() => {
-    function setStatusBarColor() {
-      const meta = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
-      if (!meta) return;
-      const color = scrollY > 100 ? '#00162D' : '#5FA7FC';
-      meta.setAttribute('content', color);
-    }
-
-    setStatusBarColor();
-  }, [scrollY]);
-
-  return (
-    <body
-    >
-      {children}
-    </body>
-  );
-};
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
-        <BodyComponent>
+        <body>
           <NextScript />
           <Main />
-        </BodyComponent>
+        </body>
       </Html>
     );
   }
 }
 
 export default MyDocument;
-function useEffects(arg0: () => void, arg1: undefined[]) {
-  throw new Error("Function not implemented.");
-}

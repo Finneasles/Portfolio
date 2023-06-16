812e9499cc977ccfa61f511698ed67bab8631f5d
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-06-16 16:03:19 +0100

    :zap: units

--- src/components/units/CTA.tsx
+++ src/components/units/CTA.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import { Button } from "@/components";
 import React from "react";
 
-export const Component = ({ }) => {
+export const Component = ({}) => {
   const { t } = useTranslation();
   return (
     <div className="flex h-[44px] items-center justify-between space-x-1 drop-shadow">
@@ -12,7 +12,7 @@ export const Component = ({ }) => {
           e.preventDefault();
           const subject = encodeURIComponent("Interested In Hiring You");
           const body = encodeURIComponent(
-            `Hi Fin,\n\nAI came across your website and I'd love to discuss potential projects with you and see if we can work together.\n\nBest regards,\n[Your Name]`
+            `Hi Fin,\n\nI hope this email finds you well. My name is [Your Name] from [Company], I recently came across your website and I'd love to discuss potential projects & ideas with you.\n\nPlease let me know if you are interested in exploring this collaboration further. I am enthusiastic about the possibility of working together and making an impact in [relevant field or industry].\n\n[Additional details]\n\nBest regards,\n[Your Name]`
           );
           window.location.href = `mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}?subject=${subject}&body=${body}`;
         }}
--- src/components/units/HeroCTA.tsx
+++ src/components/units/HeroCTA.tsx
@@ -15,7 +15,7 @@ const Component = ({ src }: { src?: string }) => {
   const letsChat = (e: { preventDefault: () => void }) => {
     const subject = encodeURIComponent("Let's Have a Chat");
     const body = encodeURIComponent(
-      `Hi Fin,\n\nI came across your website and I'd love to discuss ideas with you and have a chat.\n\nBest regards,\n[Your Name]`
+      `Hi Fin,\n\nI hope this email finds you well. My name is [Your Name] from [Company], I recently came across your website and I'd love to discuss potential projects & ideas with you.\n\nPlease let me know if you are interested in exploring this collaboration further. I am enthusiastic about the possibility of working together and making an impact in [relevant field or industry].\n\n[Additional details]\n\nBest regards,\n[Your Name]`
     );
     window.location.href = `mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}?subject=${subject}&body=${body}`;
     e.preventDefault();
@@ -24,7 +24,7 @@ const Component = ({ src }: { src?: string }) => {
   const learnMore = (e: { preventDefault: () => void }) => {
     const subject = encodeURIComponent("Let's Have a Chat");
     const body = encodeURIComponent(
-      `Hi Fin,\n\nI came across your website and I would like to learn more about you and you work.\n\nBest regards,\n[Your Name]`
+      `Hi Fin,\n\nI hope this email finds you well. My name is [Your Name] from [Company], I recently came across your website and I'd love to learn more about you and your work.\n\nBest regards,\n[Your Name]`
     );
     window.location.href = `mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}?subject=${subject}&body=${body}`;
     e.preventDefault();

4dcf988161364f40f2bb8bd2798ecbe17d4419ec
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-07-26 20:56:02 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,16 @@
-import { SessionProvider } from "next-auth/react";
 import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import { Session } from "next-auth";
 import "@/styles/globals.css";
 import i18n from "@/i18n";
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  session: Session;
-}>) {
+function MyApp({ Component, pageProps }: AppProps<{}>) {
   return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <I18nextProvider i18n={i18n}>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </I18nextProvider>
-    </SessionProvider>
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </I18nextProvider>
   );
 }
 

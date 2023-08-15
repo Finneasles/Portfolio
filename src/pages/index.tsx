9c12995be204f8ee61062e5ed2fecfd1ff840e92
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-08-15 18:27:51 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import { getAllPostsWithFrontMatter } from "@/utils";
 import { useRouter } from "next/router";
 import { Homepage } from "@/components";
 
-export default function Home({ postsData }) {
+export default function Home({ postsData, t }) {
   const router = useRouter();
-  return <Homepage posts={postsData} router={router} />;
+  return <Homepage {...{ posts: postsData, router, t }} />;
 }
 
 export async function getStaticProps() {

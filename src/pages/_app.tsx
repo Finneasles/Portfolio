182c2b9b4d0fc63d7ad0b7a5198ed8ff71fa33ea
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-08-15 18:25:49 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import { I18nextProvider } from "react-i18next";
+import { I18nextProvider, useTranslation } from "react-i18next";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import i18n from "@/i18n";
 
 function MyApp({ Component, pageProps }: AppProps<{}>) {
+  const { t } = useTranslation();
   return (
     <I18nextProvider i18n={i18n}>
       <ThemeProvider attribute="class">
-        <Component {...pageProps} />
+        <Component {...{...pageProps, t}} />
       </ThemeProvider>
     </I18nextProvider>
   );
